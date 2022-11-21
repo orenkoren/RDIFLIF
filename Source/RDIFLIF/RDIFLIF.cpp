149111a249984371d28d1c802175fc005b7c41ac
Oren <43620896+orenkoren@users.noreply.github.com>
2022-11-21 23:11:53 +0200

    Revert "3d template"
    This reverts commit 70eb75e35c2cb72fabcf44cbd9d571431bbfa174.

--- Source/RDIFLIF/RDIFLIF.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RDIFLIF.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RDIFLIF, "RDIFLIF" );
- 
