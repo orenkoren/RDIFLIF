3b390209f85482d52e64b7605bc83eb787455a77
Oren <43620896+orenkoren@users.noreply.github.com>
2022-11-21 23:16:38 +0200

    3d template with ignore uassets

---
+++ Source/RDIFLIF/RDIFLIF.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RDIFLIF.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RDIFLIF, "RDIFLIF" );
+ 
