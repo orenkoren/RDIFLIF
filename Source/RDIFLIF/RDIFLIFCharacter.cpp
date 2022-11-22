4ca071ed8b4b2a52867528e39e396bf327a26c32
benny-n <benick12@gmail.com>
2022-11-22 19:52:27 +0200

    Turn on OnReachedJumpApex notifications

--- Source/RDIFLIF/RDIFLIFCharacter.cpp
+++ Source/RDIFLIF/RDIFLIFCharacter.cpp
@@ -50,7 +50,8 @@ ARDIFLIFCharacter::ARDIFLIFCharacter()
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
 
-	// Add dynamic delegate
+	// Add dynamic delegate + turn on OnReachedJumpApex notifications
+	GetCharacterMovement()->bNotifyApex = true;
 	OnReachedJumpApex.AddDynamic(this, &ARDIFLIFCharacter::HandleOnReachedJumpApex);
 }
 
